32bfe8c1932c8d5804b851a070a3f1b7473873a5
Juan Marcos <juan.coca@sistemas.edu.bo>
2025-10-01 08:06:05 -0400

    Se agrega el proyecto

---
+++ container.py
@@ -0,0 +1,82 @@
+from tkinter import *
+import tkinter as tk
+from ventas import Ventas
+from inventario import Inventario
+from PIL import Image, ImageTk
+import sys
+import os
+
+class Container(tk.Frame):
+    def __init__(self, padre, controlador):
+        super().__init__(padre)
+        self.controlador = controlador
+        self.pack()
+        self.place(x=0 , y=0, width=800, height=400)
+        self.config(bg="#C6D9E3")
+        self.widgets()
+    
+    def  rutas(self, ruta):
+        try:
+            rutabase=sys.__MEIPASS
+        except Exception:
+            rutabase=os.path.abspath(".")
+        return os.path.join(rutabase,ruta)
+
+
+    def show_frames(self, container):
+        top_level = tk.Toplevel(self)
+        frame = container(top_level)
+        frame.config(bg="#C6D9E3")
+        frame.pack(fill="both", expand=True)
+        top_level.geometry("1100x650+120+20")
+        top_level.resizable(False, False)
+        ruta=self.rutas(r"icono.ico")
+        top_level.iconbitmap(ruta)
+
+        top_level.transient(self.master)
+        top_level.grab_set()
+        top_level.focus_set()
+        top_level.lift()
+
+    def ventas(self):
+        self.show_frames(Ventas)
+
+    def inventario(self):
+        self.show_frames(Inventario)
+
+    def widgets(self):
+
+        frame1 = tk.Frame(self, bg="#C6D9E3")
+        frame1.pack()
+        frame1.place(x=0, y=0, width=800 , height=400)
+
+        ruta=self.rutas(r"icono/btnventas.png")
+        imagen_pil = Image.open(ruta)
+        imagen_resize = imagen_pil.resize((50,50))
+        imagen_tk= ImageTk.PhotoImage(imagen_resize)
+
+        btnventas = Button(frame1, bg="#f4b400", fg="white", font="sans 18 bold",text="IR A VENTAS", command=self.ventas)
+        btnventas.config(image=imagen_tk, compound=LEFT, padx=10)
+        btnventas.image = imagen_tk
+        btnventas.place(x=500, y=30, width=240, height=60)
+
+        ruta=self.rutas(r"icono/btninventario.png")
+        imagen_pil = Image.open(ruta)
+        imagen_resize = imagen_pil.resize((50,50))
+        imagen_tk= ImageTk.PhotoImage(imagen_resize)
+
+        btninventario = Button(frame1, bg="#c62e26", fg="white",font="sans 15 bold", text="IR A INVENTARIO", command=self.inventario)
+        btninventario.config(image=imagen_tk, compound=LEFT, padx=5)
+        btninventario.image = imagen_tk
+        btninventario.place(x=500, y=130, width=240, height=60)
+
+        ruta=self.rutas(r"imagenes/tienda.png")
+        self.logo_image = Image.open(ruta)
+        self.logo_image = self.logo_image.resize((280,280))
+        self.logo_image = ImageTk.PhotoImage(self.logo_image)
+        self.logo_label = tk.Label(frame1, image=self.logo_image, bg="#C6D9E3")
+        self.logo_label.place(x=100, y=30)
+
+        copyright_label = tk.Label(frame1, text="2025 JUAN MARCOS COCA MIRANDA SIS2420", font="sans 12 bold",bg="#C6D9E3", fg="gray")
+        copyright_label.place(x=180, y=350)
+
---
+++ manager.py
@@ -0,0 +1,53 @@
+from tkinter import Tk, Frame
+from container import Container
+from ttkthemes import ThemedStyle
+import sys
+import os
+class Manager(Tk):
+    def __init__(self , *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("GESTION DE INVENTARIO")
+        self.resizable(False, False)
+        self.configure(bg="#C6D9E3")
+        self.geometry("800x400+120+20")
+        ruta=self.rutas(r"icono.ico")
+        self.iconbitmap(ruta)
+
+        self.container = Frame(self, bg="#C6D9E3")
+        self.container.pack(fill="both", expand=True)
+
+        self.frames = {
+            Container: None
+        }
+
+        self.load_frames()
+        
+        self.show_frame(Container)
+
+        self.set_theme()
+
+    def  rutas(self, ruta):
+        try:
+            rutabase=sys.__MEIPASS
+        except Exception:
+            rutabase=os.path.abspath(".")
+        return os.path.join(rutabase,ruta)
+    def load_frames(self):
+        for FrameClass in self.frames.keys():
+            frame = FrameClass(self.container, self)
+            self.frames[FrameClass] = frame
+    
+    def show_frame(self, frame_class):
+        frame = self.frames[frame_class]
+        frame.tkraise()
+
+    def set_theme(self):
+        style = ThemedStyle(self)
+        style.set_theme("breeze")
+
+def main():
+    app = Manager()
+    app.mainloop()
+
+if __name__== "__main__":
+    main()
